a50956c3731aaf2555c15fedd0798db486616268
Salman Khalil <122743479+salmankhalil12@users.noreply.github.com>
2024-08-27 17:02:13 +0500

    app.js
    Add Your New Code In JavaScript

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
-let contEle = document.querySelector(".container");
+// let contEle = document.querySelector(".container");
+// let text = ["Youtuber","Web Developer","Freelancer","Instructor"];
+// let textIndex = 0;
+// let characterIndex = 0;
+// update();
 
-let text = ["Youtuber","Web Developer","Freelancer","Instructor"];
+// function update() {
+//     characterIndex++
+//     contEle.innerHTML =`
+//         <h1>I am ${text[textIndex].slice(0,1) === "I" ? "an" : "a"} ${text[textIndex].slice(0,characterIndex)}</h1>`;
 
-let textIndex = 0;
-let characterIndex = 0;
+//         if (characterIndex === text[textIndex].length) {
+//             textIndex++
+//             characterIndex =0;
+//         }
 
-update();
+//         if (textIndex ===  text.length) {
+//             textIndex = 0;
+//         }
 
-function update() {
-    characterIndex++
-    contEle.innerHTML =`
-        <h1>I am ${text[textIndex].slice(0,1) === "I" ? "an" : "a"} ${text[textIndex].slice(0,characterIndex)}</h1>`;
+//         setTimeout(update, 500);
+// }
 
-        if (characterIndex === text[textIndex].length) {
-            textIndex++
-            characterIndex =0;
-        }
+let contEle = document.querySelector(".container")
+let text = ["red","green","blue","orange","yellow","pink","brown","perpal"];
 
-        if (textIndex ===  text.length) {
-            textIndex = 0;
-        }
+let textIndex =0;
+let addText = 0;
+contEle.style.color = text[textIndex]
+updateColor();
 
-        setTimeout(update, 500);
-}
+
+function updateColor() {
+    addText++;
+    document.body.style.backgroundColor = text[textIndex]
+    contEle.innerHTML = `
+    <h1>This Color is ${text[textIndex].slice(0,addText)};
+    `
+    if (text[textIndex] === "red" || text[textIndex === "brown"]) {
+        contEle.style.color="white";
+    }
+    if (addText === text[textIndex].length) {
+        textIndex++
+        addText =0
+    }
+    
+  
